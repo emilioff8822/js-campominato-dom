9b0c7d003888e3e155144c6ca799f3727289b7d1
emilioff8822 <emilioff@hotmail.it>
2023-03-30 17:11:42 +0200

    commit-bombs1

--- script.js
+++ script.js
@@ -1,80 +1,95 @@
+const bottoneGeneraGriglia = document.getElementById("genera-griglia");
+const contenitoreGriglia = document.getElementById("contenitore-griglia");
+let bombe = [];
 
 
-const bottoneGeneraGriglia = document.getElementById ("genera-griglia");
+// impostiamo l'evento click sul bottone Genera Griglia
+bottoneGeneraGriglia.addEventListener('click', generatoreGriglia);
 
-const contenitoreGriglia = document.getElementById ("contenitore-griglia");
+// funzione per generare la griglia
+function generatoreGriglia() {
+  contenitoreGriglia.innerHTML = ''; // svuotiamo il contenitore della griglia
 
-//event listener sul click del bottone genera-griglia
-
-bottoneGeneraGriglia.addEventListener (
-  'click' ,generatoreGriglia);
-
-
-
-
-
-  // imposto la funzione generatoreGriglia che viene richiamata al click del bottone Genera Griglia
-
-function generatoreGriglia(){
-  // in questo modo svuoto il contenitore della griglia ad ogni click
-  
-  contenitoreGriglia.innerHTML = '';
-
-  // seleziono la select con la difficolta
-
-  const selectDifficoltaGriglia = document.getElementById ("difficolta-griglia");
-
+  // selezioniamo la select con la difficoltà
+  const selectDifficoltaGriglia = document.getElementById("difficolta-griglia");
   const livelloDifficolta = selectDifficoltaGriglia.value;
-
-
 
   let numCelle = 0;
   let numRighe = 0;
   let numColonne = 0;
 
-  if (livelloDifficolta === '1'){
+  if (livelloDifficolta === '1') {
     numCelle = 100;
     numRighe = 10;
     numColonne = 10;
-
   } else if (livelloDifficolta == '2') {
     numCelle = 81;
     numRighe = 9;
     numColonne = 9;
-
-  }
-  else if (livelloDifficolta === '3') {
+  } else if (livelloDifficolta === '3') {
     numCelle = 49;
     numRighe = 7;
     numColonne = 7;
   }
 
-// creo le celle e le aggiungo al contenitore della griglia
+  // generiamo l'array delle bombe
+  bombe = [];
+  while (bombe.length < 16) {
+    const numeroCasuale = Math.floor(Math.random() * numCelle) + 1;
+    if (!bombe.includes(numeroCasuale)) {
+      bombe.push(numeroCasuale);
+    }
+  }
+  console.log("Bombe: ", bombe);
 
-for (let i =1 ; i <= numCelle; i++) {
+  // creiamo le celle e le aggiungiamo al contenitore della griglia
+  for (let i = 1; i <= numCelle; i++) {
+    const elementoGriglia = document.createElement('div');
+    elementoGriglia.classList.add("elemento-griglia");
+    elementoGriglia.innerText = i;
+    elementoGriglia.addEventListener('click', gestisciClick);
+    contenitoreGriglia.appendChild(elementoGriglia);
+  }
 
-  const elementoGriglia = document.createElement('div');
-  elementoGriglia.classList.add ("elemento-griglia")
-  elementoGriglia.innerText = i;
-  elementoGriglia.addEventListener('click', gestisciClick);
-  contenitoreGriglia.appendChild(elementoGriglia);
-
+  // impostiamo le dimensioni del contenitore della griglia in base al numero di righe e colonne
+  contenitoreGriglia.style.gridTemplateColumns = `repeat(${numColonne}, 1fr)`;
+  contenitoreGriglia.style.gridTemplateRows = `repeat(${numRighe}, 1fr)`;
 }
 
-// impostiamo le dimensioni del contenitore della griglia in base al numero di righe e colonne
-
-contenitoreGriglia.style.gridTemplateColumns = `repeat(${numColonne}, 1fr)`;
-contenitoreGriglia.style.gridTemplateRows = `repeat(${numRighe}, 1fr)`;
-}
-
-
-// definiamo la funzione gestisciClick che avviene per ogni click della cella
-
+// funzione per gestire il click su una cella
 function gestisciClick(event) {
   const elementoCliccato = event.target;
-  elementoCliccato.classList.add('clicked');
-
   const elementoG = elementoCliccato.innerText;
 
-  console.log("Hai cliccato sulla cella numero : " + elementoG);
+  if (bombe.includes(parseInt(elementoG))) {
+    // abbiamo cliccato su una bomba, terminiamo la partita
+    alert("Hai preso una bomba! Gioco terminato.");
+    elementoCliccato.style.backgroundColor = "red";
+    contenitoreGriglia.removeEventListener('click', gestisciClick);
+
+    // resettiamo la griglia per una nuova partita
+    contenitoreGriglia.innerHTML = '';
+    bombe = [];
+  } else {
+    //quando  la cella cliccata non è una bomba, la coloriamo di azzurro
+    console.log("Hai cliccato sulla cella numero: " + elementoG);
+    elementoCliccato.classList.add('clicked');
+    elementoCliccato.style.backgroundColor = "lightblue";
+
+    // controllo se l'utente ha vinto la partita
+    const celleCliccate = document.querySelectorAll('.clicked');
+    const numCelleCliccate = celleCliccate.length;
+    const numCelleSenzaBomba = contenitoreGriglia.children.length - bombe.length;
+
+    if (numCelleCliccate === numCelleSenzaBomba) {
+      alert("Complimenti, hai vinto! Hai trovato tutte le celle che non erano delle bombe.");
+      contenitoreGriglia.removeEventListener('click', gestisciClick);
+
+      // reset griglia
+      contenitoreGriglia.innerHTML = '';
+      bombe = [];
+    }
+  }
 }
+
+
